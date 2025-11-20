ee6eee088144b920a1e1f6c8b04c4cd74aa25e00
anthony0tran <anthony0tran@gmail.com>
2025-11-20 20:58:50 +0100

    add WeekNumber.Tests project

---
+++ WeekNumber.Tests/BrushHelperTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using Shouldly;
+using WeekNumber.Helpers;
+using Xunit;
+
+namespace WeekNumber.Tests;
+
+public class BrushHelperTests
+{
+    [Fact]
+    public void GetBrushFromColor_ReturnsPredefinedBrush_WhenColorIsKnown()
+    {
+        // Arrange
+        var color = Color.Red;
+
+        // Act
+        var brush = BrushHelper.GetBrushFromColor(color);
+
+        // Assert
+        brush.ShouldBeOfType<SolidBrush>();
+        ((SolidBrush)brush).Color.ShouldBe(color);
+        brush.ShouldBeSameAs(Brushes.Red);
+    }
+
+    [Fact]
+    public void GetBrushFromColor_ReturnsNewSolidBrush_WhenColorIsCustom()
+    {
+        // Arrange
+        var customColor = Color.FromArgb(123, 45, 67);
+
+        // Act
+        var brush = BrushHelper.GetBrushFromColor(customColor);
+
+        // Assert
+        brush.ShouldBeOfType<SolidBrush>();
+        ((SolidBrush)brush).Color.ShouldBe(customColor);
+        brush.ShouldNotBeSameAs(Brushes.Red);
+        brush.ShouldNotBeSameAs(Brushes.Blue);
+        brush.ShouldNotBeSameAs(Brushes.Green);
+    }
+}
