c9b706d2433500f780e777d3a2a2b4213c685879
Shanko100 <s.hamidi@tuta.io>
2026-04-10 13:12:12 +0200

    Update NotificationAreaIcon.cs

--- WeekNumber/NotificationAreaIcon.cs
+++ WeekNumber/NotificationAreaIcon.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+using System.Drawing;
 using System.Globalization;
 using Microsoft.Win32;
 using WeekNumber.Forms;
@@ -19,7 +19,14 @@ public sealed class NotificationAreaIcon : IDisposable
     private SolidBrush _currentBrush = BrushHelper.GetBrushFromColor(Properties.Settings.Default.SelectedColor);
     private const string DefaultFontFamily = "Arial";
     private Font _font = new(DefaultFontFamily, IconSizeInPixels, _currentFontStyle, GraphicsUnit.Pixel);
+    private Icon? _currentIcon;
     private ColorDialog? _colorDialog;
+
+    private static FontStyle ValidateFontStyle(int raw)
+    {
+        const FontStyle allowed = FontStyle.Bold | FontStyle.Italic | FontStyle.Strikeout;
+        return (FontStyle)(raw & (int)allowed);
+    }
 
     public static NotificationAreaIcon Instance => _instance.Value;
 
@@ -48,9 +55,11 @@ public sealed class NotificationAreaIcon : IDisposable
 
             if (_colorDialog.ShowDialog() == DialogResult.OK)
             {
+                var oldBrush = _currentBrush;
                 _currentBrush = BrushHelper.GetBrushFromColor(_colorDialog.Color);
                 Properties.Settings.Default.SelectedColor = _colorDialog.Color;
                 Properties.Settings.Default.Save();
+                oldBrush.Dispose();
                 UpdateIcon();
             }
 
